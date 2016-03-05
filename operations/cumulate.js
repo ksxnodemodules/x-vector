9c79e63e28a20956b1044a13a5aa902c2b5234c3
khai96_ <hvksmr1996@gmail.com>
2016-03-05 20:36:29 +0700

    upgrade idea

--- operations/cumulate.js
+++ operations/cumulate.js
@@ -1,3 +1,24 @@
 
-module.exports = (operate, identity) =>
-	(...operands) => operands.reduce(operate, identity);
+((module) => {
+	'use strict';
+
+	var cumulate = (operate, identity) =>
+		(...operands) => operands.reduce(operate, identity);
+
+	var search = (check, identity, fres, ires) => (...operands) => {
+		for (let element of operands) {
+			let res = ires(element);
+			if (check(res)) {
+				return fres(pres);
+			}
+		}
+	};
+
+	const DEFAULT = (x) => x;
+
+	cumulate.search = (check, identity, fres, ires) =>
+		search(check, identity, fres || DEFAULT, ires || DEFAULT);
+
+	module.exports = cumulate;
+
+})(module);
