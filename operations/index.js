5fef31600d893f95fc7c30d0b4d9e9a92056e147
khai96_ <hvksmr1996@gmail.com>
2016-03-06 12:48:42 +0700

    fix

--- operations/index.js
+++ operations/index.js
@@ -3,7 +3,7 @@
 	'use strict';
 
 	module.exports = Object.freeze({
-		BITS_OPERATIONS: require('./bit-operations.js'),
+		BITS_OPERATIONS: require('./bits-operations.js'),
 		BOOLEAN_OPERATIONS: require('./boolean-operations.js'),
 		NUMBER_OPERATIONS: require('./number-operations.js')
 	});
