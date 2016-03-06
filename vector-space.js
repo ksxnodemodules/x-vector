f0f0b0d7e74c93de15c928939e9d4ae2f2e230d7
khai96_ <hvksmr1996@gmail.com>
2016-03-06 14:03:37 +0700

    complete

--- vector-space.js
+++ vector-space.js
@@ -26,34 +26,69 @@
 			}
 
 			substract(vector) {
-				return this.add(vector.negative());
+				return this.add(vector.clone().negative());
 			}
 
 			multiply(scalar) {
-				for (let i = 0; i != dimensions; ++i) {
+				for (let i = 0; i !== dimensions; ++i) {
 					this.set(i, times(this.get(i), vector.get(i)));
 				}
 				return this;
 			}
 
-			scalarDivide(scalar) {
-				return this.multiply(scalar.inverse());
+			divide(scalar) {
+				return this.multiply(scalar.clone().inverse());
 			}
 
-			dot1v(vector) {
+			dot(vector) {
 				return Vector.dot2v(this, vector);
+			}
+
+			slash(vector) {
+				return Vector.slash2v(this, vector);
+			}
+
+			static sum2v(lvec, rvec) {
+				return new this().fill(zero).add(lvec).add(rvec);
+			}
+
+			static sum(...vlist) {
+				var result = new this().fill(zero);
+				vlist.forEach((vector) => result.add(vector));
+				return result;
+			}
+
+			static inverse(vector) {
+				return vector.clone().inverse();
+			}
+
+			static multiply(vector, scalar) {
+				return new this().assign(vector).multiply(scalar);
 			}
 
 			static dot2v(lvec, rvec) {
 				var result = zero;
-				for (let i = 0; finite(result) && i != dimensions; ++i) {
+				for (let i = 0; finite(result) && i !== dimensions; ++i) {
 					result = plus(result, times(lvec.get(i), rvec.get(i)));
 				}
 				return result;
 			}
 
+			static slash2v(lvec, rvec) {
+				return this.dot2v(lvec, rvec.clone().inverse());
+			}
+
 			static dot(...vlist) {
-				var result = zero;
+				var sum = zero;
+				for (let i = 0; finite(sum) && i !== dimensions; ++i) {
+					let product = one;
+					let length = vlist.length;
+					for (let j = 0; nonzero(product) && j !== length; ++j) {
+						product = times(product, vlist[j].get(i));
+					}
+					sum = plus(sum, product);
+				}
+				return sum;
 			}
 
 			static create(...args) {
